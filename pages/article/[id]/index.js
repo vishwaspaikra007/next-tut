fd38a106e881d10fa385dae089083d2c07ea21b3
Vishwas Paikra <vishwaspaikra007@gmail.com>
2022-06-06 12:10:57 +0530

    JSON error?

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -21,7 +21,12 @@ export default function Article({article}) {
 //  getServersideProps
 export const getStaticProps = async (context) => {
   // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
-  const res = await fetch(server + '/api/articles/' + context.params.id)
+  const res = await fetch(server + '/api/articles/' + context.params.id, {
+    headers: {
+      'Accept': 'application/json',
+      'User-Agent': '*',
+    }
+  })
   const article = await res.json()
 
   return {
@@ -33,7 +38,12 @@ export const getStaticProps = async (context) => {
 
 export const getStaticPaths = async () => {
   // const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-  const res = await fetch(server + '/api/articles')
+  const res = await fetch(server + '/api/articles', {
+    headers: {
+      'Accept': 'application/json',
+      'User-Agent': '*',
+    }
+  })
   const articles = await res.json()
 
   const ids = articles.map(article => article.id)
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import ArticleList from '../components/ArticleList'
 import { server } from '../config'
 
 export default function Home({articles}) {
-  console.log(articles)
   return (
     <div>
       {/* <Head>
@@ -17,7 +16,12 @@ export default function Home({articles}) {
 
 export const getStaticProps = async () => {
   // const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
-  const res = await fetch(server + '/api/articles')
+  const res = await fetch(server + '/api/articles', {
+    headers: {
+      'Accept': 'application/json',
+      'User-Agent': '*',
+    }
+  })
   const articles = await res.json()
 
   return {
